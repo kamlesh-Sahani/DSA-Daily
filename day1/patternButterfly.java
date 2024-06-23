125145c3d7a1ccdb8cbd755c66797ab29c38bee9
kamlesh-Sahani <piyush.7434@gmail.com>
2024-06-23 23:55:47 +0530

    day1

---
+++ day1/patternButterfly.java
@@ -0,0 +1,7 @@
+package day1;
+
+public class patternButterfly {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ day1/patternSolidRectangle.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class patternSolidRectangle {
+    public static void main(String args[]){
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5;j++){
+                System.out.print("*");
+            } 
+            System.out.println("");
+        }
+    }
+}
