4b2b45fb45da115bddcef7c6c8ef67c68ee0462b
kamlesh sahani <alex20150001969@gmail.com>
2024-09-10 23:18:43 +0530

    day16

---
+++ day16/singleElement.js
@@ -0,0 +1,8 @@
+var singleNonDuplicate = function(nums) {
+    //[1,1,2,3,3,4,4,8,8]->
+    let mid,start=0 = nums.length;
+    while(start<=end){
+        mid = Math.floor((start+end)/2);
+
+    }
+};                                       
